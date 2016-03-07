b77229429f21b4002bde80a339ae7674f9cfb61d
xu1912 <xux1912@gmail.com>
2016-03-07 14:00:52 -0600

    Update causal_gen.sh

--- causal_gen.sh
+++ causal_gen.sh
@@ -7,6 +7,6 @@ let nc=$4
 for i in `seq 1 $num_reg`;do
 	if [ -f $BASIS_DIR"/h_legend_maf/"$i".txt" ];then
 		echo $i
-		Rscript $BASIS_DIR/bin/causal_allel.R $BASIS_DIR"/h_legend_maf/"$i".txt" $prev $nc
+		Rscript $BASIS_DIR/bin/causal_allel.R $BASIS_DIR"/h_legend_maf/"$i".txt" $BASIS_DIR"/causal_list/"$i".txt" $prev $nc
 	fi
 done
