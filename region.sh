4e6f33a5cefa3b6dc808b80f59031d1bcbc0181a
xu1912 <xux1912@gmail.com>
2016-03-07 13:15:46 -0600

    Parameterization
    Update code to accept parameters from command line.

--- region.sh
+++ region.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-OUT_DIR="/DataStorage/Backup/xc/ngs/region"
+hap_ref=$1                          ##SNP Reference: Eur.legend
+num_reg=$2                          ##Number of regions created
+let REGION_LN=$3                    ##Region length
+OUT_DIR=$4                          ##Region file directory
 mkdir -p $OUT_DIR
-let REGION_LN=100000
-hap_ref="/home/xc/ngs/hap_ref"
-f=$hap_ref"/chr22_EUR.legend"
 
-for i in `seq 1001 1003`;do
-echo $i
-cl=`shuf -i 311-308149 -n 1`
-mp=`sed -n "$cl p" $f | cut -f2`
-mp_n=`sed -n "$cl p" $f | cut -f1`
-mp_af=`sed -n "$cl p" $f | cut -f5`
-tl=`shuf -i 1-$REGION_LN -n 1`
 
-let LOW_BOUND=$(($mp - $tl))
-let UP_BOUND=$(($LOW_BOUND + $REGION_LN - 1))
-let LOW_BOUND_L=$(($LOW_BOUND - 500))
-let UP_BOUND_R=$(($UP_BOUND + 500))
+for i in `seq 1 $num_reg`;do
+  echo $i
+  cl=`shuf -i 311-308149 -n 1`
+  mp=`sed -n "$cl p" $hap_ref | cut -f2`
+  mp_n=`sed -n "$cl p" $hap_ref | cut -f1`
+  mp_af=`sed -n "$cl p" $hap_ref | cut -f5`
+  tl=`shuf -i 1-$REGION_LN -n 1`
 
-echo "LOW_BOUND:"$LOW_BOUND > $OUT_DIR/$i.txt
-echo "UP_BOUND:"$UP_BOUND >> $OUT_DIR/$i.txt
-echo "disease_pos:"$mp >> $OUT_DIR/$i.txt
-echo "disease_allele:"$mp_n >> $OUT_DIR/$i.txt
-echo "disease_af:"$mp_af >> $OUT_DIR/$i.txt
+  let LOW_BOUND=$(($mp - $tl))
+  let UP_BOUND=$(($LOW_BOUND + $REGION_LN - 1))
+  let LOW_BOUND_L=$(($LOW_BOUND - 500))
+  let UP_BOUND_R=$(($UP_BOUND + 500))
+
+  echo "LOW_BOUND:"$LOW_BOUND > $OUT_DIR/$i.txt
+  echo "UP_BOUND:"$UP_BOUND >> $OUT_DIR/$i.txt
 done
