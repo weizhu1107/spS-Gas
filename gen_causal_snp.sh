ce454dc906242135e1ae99130defcf8c9546ffd1
Chao Xu <xux1912@gmail.com>
2016-04-25 13:31:05 -0500

    Update gen_causal_snp.sh

--- gen_causal_snp.sh
+++ gen_causal_snp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 BASIS_DIR=$1
-num_reg=$2
-let prev=$3
-let nc=$4
+num_reg=$2			##number of regions
+let prev=$3			##prevalence
+let nc=$4			##number of causal alleles
+mkdir -p $BASIS_DIR/causal_list
 
 for i in `seq 1 $num_reg`;do
-	if [ -f $BASIS_DIR"/h_legend_maf/"$i".txt" ];then
+	if [ -f $BASIS_DIR"/legend_maf/"$i".txt" ];then
 		echo $i
 		Rscript $BASIS_DIR/bin/causal_allel.R $BASIS_DIR"/legend_maf/"$i".txt" $BASIS_DIR"/causal_list/"$i".txt" $prev $nc
 	fi
