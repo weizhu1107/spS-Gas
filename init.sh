8f26ec45ae9009062919e3a86148141613a4d8f2
Chao Xu <xux1912@gmail.com>
2016-04-25 14:37:14 -0500

    Update and rename init_region.sh to init.sh

--- init.sh
+++ init.sh
@@ -2,10 +2,15 @@
 hap_ref=$1                          ##SNP Reference: Eur.legend
 num_reg=$2                          ##Number of regions created
 let REGION_LN=$3                    ##Region length
-REG_DIR=$4/region                   ##Region file directory
+BASIS_DIR=$4
+let prev=$5			##prevalence
+let nc=$6			##number of causal alleles
+
+REG_DIR=$BASIS_DIR/region                   ##Region file directory
 mkdir -p $REG_DIR
-LGD_DIR=$4/legend_maf               ##Legend file directory
+LGD_DIR=$BASIS_DIR/legend_maf               ##Legend file directory
 mkdir -p $LGD_DIR
+mkdir -p $BASIS_DIR/causal_list
 
 for i in `seq 1 $num_reg`;do
   echo $i
@@ -24,4 +29,12 @@ for i in `seq 1 $num_reg`;do
   echo "UP_BOUND:"$UP_BOUND >> $REG_DIR/$i.txt
   
   ./init_legend_dir.py $1 $LOW_BOUND $UP_BOUND $LGD_DIR/$i
+done
+
+
+for i in `seq 1 $num_reg`;do
+	if [ -f $LGD_DIR"/"$i".txt" ];then
+		echo $i
+		Rscript $BASIS_DIR/scripts/causal_allel.R $LGD_DIR"/"$i".txt" $BASIS_DIR"/causal_list/"$i".txt" $prev $nc
+	fi
 done
