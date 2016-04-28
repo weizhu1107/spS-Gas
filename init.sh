6b2b33beff41c2e20db7f4f3295d1a8df626aa8c
Chao Xu <xux1912@gmail.com>
2016-04-28 13:51:05 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,19 +5,20 @@ while read line;do
         count=$(( $count + 1 ))
 done < $1
 
-hap_ref=${a[1]}                          ##SNP Reference: Eur.legend
-num_reg=${a[4]}                          ##Number of regions created
-let REGION_LN=${a[5]}                    ##Region length
-BASIS_DIR=${a[2]}			##Basis_directory_for_output
-BIN_DIR=${a[3]}				##Basis_directory_for_bin
-prev=${a[6]}				##Prevalence
-let nc=${a[9]}				##Number of causal alleles
+num_reg=${a[3]}                          ##Number of regions created
+let REGION_LN=${a[4]}                    ##Region length
+OUT_BASIS_DIR=${a[1]}			##Basis_directory_for_output
+BASIS_DIR=${a[2]}				##Basis_directory_for_bin
+prev=${a[5]}				##Prevalence
+let nc=${a[8]}				##Number of causal alleles
 
-REG_DIR=$BASIS_DIR/region                   ##Region file directory
+hap_ref=$BASIS_DIR"/hap_ref"
+REG_DIR=$OUT_BASIS_DIR/region                   ##Region file directory
 mkdir -p $REG_DIR
-LGD_DIR=$BASIS_DIR/legend_maf               ##Legend file directory
+LGD_DIR=$OUT_BASIS_DIR/legend_maf               ##Legend file directory
 mkdir -p $LGD_DIR
-mkdir -p $BASIS_DIR/causal_list
+CAL_DIR=$OUT_BASIS_DIR/causal_list
+mkdir -p $CAL_DIR
 
 f=$hap_ref"/chr22_EUR.legend"
 
@@ -37,13 +38,13 @@ for i in `seq 1 $num_reg`;do
   echo "LOW_BOUND:"$LOW_BOUND > $REG_DIR/$i.txt
   echo "UP_BOUND:"$UP_BOUND >> $REG_DIR/$i.txt
   
-  $BIN_DIR/script/init_legend_dir.py $f $LOW_BOUND $UP_BOUND $LGD_DIR/$i
+  $BASIS_DIR/script/init_legend_dir.py $f $LOW_BOUND $UP_BOUND $LGD_DIR/$i
 done
 
 
 for i in `seq 1 $num_reg`;do
 	if [ -f $LGD_DIR"/"$i".txt" ];then
 		echo $i
-		Rscript $BIN_DIR/script/causal_allel.R $LGD_DIR"/"$i".txt" $BASIS_DIR"/causal_list/"$i".txt" $prev $nc
+		Rscript $BASIS_DIR/script/causal_allel.R $LGD_DIR"/"$i".txt" $CAL_DIR"/"$i".txt" $prev $nc
 	fi
 done
