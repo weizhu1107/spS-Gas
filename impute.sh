42bfb8029a6ed881a758c31f5da0000275333559
Chao Xu <xux1912@gmail.com>
2016-04-29 11:00:45 -0500

    Update impute.sh

--- impute.sh
+++ impute.sh
@@ -5,7 +5,8 @@ while read line;do
         count=$(( $count + 1 ))
 done < $1
 
-OUT_BASIS_DIR=${ta[1]}	##Basis_directory_for_output
+OUT_BASIS_DIR=${ta[1]}	                                ##Basis_directory_for_output
+BASIS_DIR=${ta[2]}	                                ##Basis_directory_for_spS-Gas
 SN=$4
 
 REGION_FL=$OUT_BASIS_DIR"/region/"$SN".txt"	        ##Region file location
@@ -27,18 +28,18 @@ if [ -d $OUT_BASIS_DIR/$2/$SN ];then
 
         if [ ! -f $OUT_BASIS_DIR/$2/$SN/impute_ref.gen.gz ];then
                 cd $OUT_BASIS_DIR/$2/$SN
-                /home/xc/ngs/bin/vcf_to_gen.pl -vcf chr22.vcf -gen impute_ref.gen
+                $BASIS_DIR/bin/vcf_to_gen.pl -vcf chr22.vcf -gen impute_ref.gen
         fi
 
         echo "Process study..."
 
         if [ ! -f $OUT_BASIS_DIR/$3/$SN/impute_ref.gen.gz ];then
                 cd $OUT_BASIS_DIR/$3/$SN
-                /home/xc/ngs/bin/vcf_to_gen.pl -vcf chr22.vcf -gen impute_study.gen
+                $BASIS_DIR/bin/vcf_to_gen.pl -vcf chr22.vcf -gen impute_study.gen
         fi
 
         echo "Imputing..."
         mkdir -p $OUT_BASIS_DIR/impute/$2_$3/$SN
-        impute2 -m /home/xc/ngs/hap_ref/chr22_combined_b37.txt -g_ref $OUT_BASIS_DIR/$2/$SN/impute_ref.gen.gz -g $OUT_BASIS_DIR/$3/$SN/impute_study.gen.gz -buffer 1 -int $LOW_BOUND $UP_BOUND -burnin 9 -iter 28 -o $OUT_BASIS_DIR/impute/$2_$3/$SN/impute
+        impute2 -m $BASIS_DIR/hap_ref/chr22_combined_b37.txt -g_ref $OUT_BASIS_DIR/$2/$SN/impute_ref.gen.gz -g $OUT_BASIS_DIR/$3/$SN/impute_study.gen.gz -buffer 1 -int $LOW_BOUND $UP_BOUND -burnin 9 -iter 28 -o $OUT_BASIS_DIR/impute/$2_$3/$SN/impute
 
 fi
