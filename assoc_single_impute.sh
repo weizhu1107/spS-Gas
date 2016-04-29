5819b90d7f813e31949dbcb91adb17baf51b2f56
Chao Xu <xux1912@gmail.com>
2016-04-29 15:15:22 -0500

    Update and rename assoc_single_impute_git.sh to assoc_single_impute.sh

---
+++ assoc_single_impute.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+count=0
+while read line;do
+        ta[$count]=${line##*=}
+        count=$(( $count + 1 ))
+done < $1
+
+OUT_BASIS_DIR=${ta[1]}	                                ##Basis_directory_for_output
+BASIS_DIR=${ta[2]}	                                ##Basis_directory_for_spS-Gas
+SN=$3
+
+data_dir=$OUT_BASIS_DIR/impute/$2/$SN
+
+cd $data_dir
+
+if [ ! -f "impute_assoc.ped" ];then
+	cd ../
+	IFS='_' read -a array <<< "$1"
+	mp="${array[3]}_${array[4]}_${array[5]}"
+	mkdir -p tmp
+	cp $OUT_BASIS_DIR/$mp/1/impute_study.gen.samples tmp/
+	$BASIS_DIR/script/impute_ped.py tmp/impute_study.gen.samples
+	cp tmp/impute_assoc.ped $2/
+	rm -rf tmp
+fi
+
+if [ -f "impute_assoc.ped" ];then
+	plink --noweb --file impute_assoc --allow-no-sex --logistic --no-sex --no-fid --no-parents
+	rm -f plink.nosex &
+fi
