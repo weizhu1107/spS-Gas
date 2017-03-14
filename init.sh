58d97a4e6e05d4d75ce4fbce9166390e2f39886c
weizhu1107 <zhuw05@126.com>
2017-03-14 15:19:36 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-count=0
-while read line;do
-        a[$count]=${line##*=}
-        count=$(( $count + 1 ))
-done < $1
 
-num_reg=${a[3]}                          ##Number of regions created
-let REGION_LN=${a[4]}                    ##Region length
-OUT_BASIS_DIR=${a[1]}			##Basis_directory_for_output
-BASIS_DIR=${a[2]}				##Basis_directory_for_bin
+num_reg=1000                          ##Number of regions created
+let REGION_LN=100000                    ##Region length
+OUT_BASIS_DIR="/extra/wzhu3/genotype_imputation/Output/"			##Basis_directory_for_output
+BASIS_DIR="/extra/wzhu3/genotype_imputation/"				##Basis_directory_for_bin
 
-hap_ref=$BASIS_DIR"/hap_ref"
+hap_ref=$BASIS_DIR"/1000Genome"
 REG_DIR=$OUT_BASIS_DIR/region                   ##Region file directory
 mkdir -p $REG_DIR
 LGD_DIR=$OUT_BASIS_DIR/legend_maf               ##Legend file directory
 mkdir -p $LGD_DIR
 
 
-f=$hap_ref"/chr22_EUR.legend"
+f=$hap_ref"/1000GP_CHR22_EUR.legend"
 
 for i in `seq 1 $num_reg`;do
   echo $i
@@ -33,5 +28,5 @@ for i in `seq 1 $num_reg`;do
   echo "LOW_BOUND:"$LOW_BOUND > $REG_DIR/$i.txt
   echo "UP_BOUND:"$UP_BOUND >> $REG_DIR/$i.txt
   
-  $BASIS_DIR/script/init_legend_dir.py $f $LOW_BOUND $UP_BOUND $LGD_DIR/$i
+  $BASIS_DIR/bin/init_legend_dir.py $f $LOW_BOUND $UP_BOUND $LGD_DIR/$i
 done
